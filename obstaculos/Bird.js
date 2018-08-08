88669193ed9743d5fd80d16c409eab2d61c73507
Kevin Louis <keviinlouiis@gmail.com>
2018-08-08 11:34:43 -0300

    obstaculos como classes

--- obstaculos/Bird.js
+++ obstaculos/Bird.js
@@ -1,25 +1,9 @@
-function Bird(){
-    this.altura = 20;
-    this.largura = 30;
-
-    this.posicaoY = FLOOR_POS_Y - this.altura - 60;
-    this.posicaoX = width;
-
-    this.isDead = false;
-
-    this.display = function(){
-        fill(255, 153, 59);
-        noStroke();
-        rect(this.posicaoX, this.posicaoY, this.largura, this.altura);
-    };
-
-    this.update = function () {
-        this.posicaoX -= GAMESPEED;
-        this.display();
-        this.checkIsDead();
-    };
-
-    this.checkIsDead = function () {
-        this.isDead = this.posicaoX <= (this.largura*-1);
+class Bird extends Obstaculo{
+    constructor(){
+        super();
+        this.altura = 20;
+        this.largura = 30;
+    
+        this.posicaoY = FLOOR_POS_Y - this.altura - 60;
     }
 }
